1ad5c2541444d6721555775f8231e6791e7d9d20
matthewjhough <matthough1@icloud.com>
2018-08-16 16:48:53 -0400

    update to basic observer pattern

--- watchable.js
+++ watchable.js
@@ -12,10 +12,39 @@ function View(controller) {
     this.heading = document.getElementById('heading');
     this.heading.innerText = controller.getModelHeading();
     this.heading.addEventListener('click', controller);
+    this.update = function(data) {
+        this.heading.innerText = data.heading;
+    }
+    this.controller.model.registerObserver(this);
 }
 
 function Model() {
-    this.heading = 'hello';
+    var self = this;
+    var heading = 'hello';
+    // collection of observers
+    this.observers = [];
+    // add to collection of observers
+    this.registerObserver = function(observer) {
+        self.observers.concat(observer);
+    }
+    // Iterate through observers, calling their update method
+    this.notifyAll = function() {
+        self.observers.forEach(function(observer) {
+            observer.update(self);
+        });
+    }
+
+    // Pass this, as its the object we want to affect. Heading is the   
+    // name of the property we want it to be attached to. Then we 
+    // define the accessor and assignment functions
+    Object.defineProperty(this,"heading",{
+        get: function() { return heading; },
+        set: function(value) { 
+        heading = value; 
+        //call notifyAll in the assignment function     
+        this.notifyAll();
+        }
+    });
 }
 
 function Controller(model) {
@@ -42,6 +71,7 @@ function Controller(model) {
     // Change Model
     this.clickHandler = function(target) {
         self.model.heading = 'World';
-        target.innerText = self.getModelHeading();
+        // notify all subscriptions
+        // self.model.notifyAll();
     }
 }
